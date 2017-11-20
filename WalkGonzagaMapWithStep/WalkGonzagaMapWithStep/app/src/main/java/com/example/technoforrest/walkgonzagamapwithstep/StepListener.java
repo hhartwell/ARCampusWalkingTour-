48cefa5e401ded2b669a7a25f993aae4b8cbcea3
technoforrest <31766195+technoforrest@users.noreply.github.com>
2017-11-19 18:31:27 -0800

    working directions with current location

---
+++ WalkGonzagaMapWithStep/WalkGonzagaMapWithStep/app/src/main/java/com/example/technoforrest/walkgonzagamapwithstep/StepListener.java
@@ -0,0 +1,13 @@
+package com.example.technoforrest.walkgonzagamapwithstep;
+
+/**
+ * Created by Danielle on 11/9/2017.
+ */
+
+// Will listen to step alerts
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
+
