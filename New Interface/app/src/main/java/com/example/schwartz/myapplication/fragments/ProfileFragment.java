5422a657d4345d936a501aee4aa9d57a3fd846bf
Holly Schwartz <schwartz@Hollys-MacBook-Pro.local>
2018-02-18 13:18:18 -0800

    update

---
+++ New Interface/app/src/main/java/com/example/schwartz/myapplication/fragments/ProfileFragment.java
@@ -0,0 +1,33 @@
+package com.example.schwartz.myapplication.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.schwartz.myapplication.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_awards, container, false);
+    }
+}
