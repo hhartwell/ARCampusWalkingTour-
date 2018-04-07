cae67da29a82a4491d6f9bb518255e0b5a77bfec
hhartwell <hhartwell@zagmail.gonzaga.edu>
2018-04-06 19:00:26 -0700

    Gallery fragment has been updated to work with more than one tour

--- New Interface/app/src/main/java/com/example/schwartz/myapplication/GalleryFragment.java
+++ New Interface/app/src/main/java/com/example/schwartz/myapplication/GalleryFragment.java
@@ -1,14 +1,18 @@
 package com.example.schwartz.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -33,7 +37,6 @@ public class GalleryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -41,18 +44,21 @@ public class GalleryFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
 
         // add all images into the fragment
-        assignAllPicturesToGalleryFragment(rootView);
-        //formatAllImageButtonSizes(rootView);
+        ArrayList<ImageButton> imageButtons = assignAllPicturesToGalleryFragment(rootView);
+        GridLayout gridLayout = (GridLayout) rootView.findViewById(R.id.grid_layout);
+        for (int i = 0; i < imageButtons.size(); i++){
+            gridLayout.addView(imageButtons.get(i));
+        }
         // retrieve button resources
-        ImageButton desmet = (ImageButton) rootView.findViewById(R.id.desmet);
-        desmet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // needs to be fixed to be a true back button
-                Intent intent = new Intent(view.getContext(), DesmetActivity.class);
-                startActivity(intent);
-            }
-        });
+//        ImageButton desmet = (ImageButton) rootView.findViewById(R.id.desmet);
+//        desmet.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // needs to be fixed to be a true back button
+//                Intent intent = new Intent(view.getContext(), DesmetActivity.class);
+//                startActivity(intent);
+//            }
+//        });
         return rootView;
     }
 /*
@@ -94,59 +100,23 @@ public class GalleryFragment extends Fragment {
     }
 */
 
+
     /**
      * helper function that adds all images from the WalkGU server to all
      *  gallery image buttons in the fragment
      * @param rootView
      */
-    private void assignAllPicturesToGalleryFragment(View rootView){
-
+    private ArrayList<ImageButton> assignAllPicturesToGalleryFragment(View rootView){
         //ArrayList<String> dormURLS = new ArrayList(Arrays.asList(getContext().getResources().getStringArray(R.array.dorm_gallery_urls)));
         ArrayList<ImageButton> imageButtons = new ArrayList<ImageButton>();
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.alliance));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.catherinemonica));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.chardin));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.corkery));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.coughlin));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.crimont));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.cushing));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.desmet));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.dillon));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.dooley));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.dussault));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.goller));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.kennedy));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.lincoln));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.madonna));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.marian));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.roncalli));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.sharp));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.twohy));
-        imageButtons.add((ImageButton) rootView.findViewById(R.id.welch));
+        ImageButton ib;
+        for (int i = 0; i < getContext().getResources().getStringArray(R.array.dorm_gallery_urls).length; i++){
+            imageButtons.add(new ImageButton(getContext()));
+        }
 
         assignDrawablesToImageButtons(imageButtons, getDormDrawables(getContext().getResources().getStringArray(R.array.dorm_gallery_urls)));
-
-//        retrieveGalleryItem(dormURLS.get(0),rootView, R.id.alliance);
-//        retrieveGalleryItem(dormURLS.get(1), rootView, R.id.catherinemonica);
-//        retrieveGalleryItem(dormURLS.get(2), rootView, R.id.chardin);
-//        retrieveGalleryItem(dormURLS.get(3), rootView, R.id.corkery);
-//        retrieveGalleryItem(dormURLS.get(4), rootView, R.id.coughlin);
-//        retrieveGalleryItem(dormURLS.get(5), rootView, R.id.crimont);
-//        retrieveGalleryItem(dormURLS.get(6), rootView, R.id.cushing);
-//        retrieveGalleryItem(dormURLS.get(7), rootView, R.id.desmet);
-//        retrieveGalleryItem(dormURLS.get(8), rootView, R.id.dillon);
-//        retrieveGalleryItem(dormURLS.get(9), rootView, R.id.dooley);
-//        retrieveGalleryItem(dormURLS.get(10), rootView, R.id.dussault);
-//        retrieveGalleryItem(dormURLS.get(11), rootView, R.id.goller);
-//        retrieveGalleryItem(dormURLS.get(12), rootView, R.id.kennedy);
-//        retrieveGalleryItem(dormURLS.get(13), rootView, R.id.lincoln);
-//        retrieveGalleryItem(dormURLS.get(14), rootView, R.id.madonna);
-//        retrieveGalleryItem(dormURLS.get(15), rootView, R.id.marian);
-//        retrieveGalleryItem(dormURLS.get(16), rootView, R.id.roncalli);
-//        retrieveGalleryItem(dormURLS.get(17), rootView, R.id.sharp);
-//        retrieveGalleryItem(dormURLS.get(18), rootView, R.id.twohy);
-//        retrieveGalleryItem(dormURLS.get(19), rootView, R.id.welch);
-
+        return imageButtons;
+/*
         ArrayList<String> dormURLS = new ArrayList(Arrays.asList(getContext().getResources().getStringArray(R.array.dorm_gallery_urls)));
         retrieveGalleryItem(dormURLS.get(0),rootView, R.id.alliance);
         retrieveGalleryItem(dormURLS.get(1), rootView, R.id.catherinemonica);
@@ -160,16 +130,17 @@ public class GalleryFragment extends Fragment {
         //retrieveGalleryItem(dormURLS.get(9), rootView, R.id.dooley);
         //retrieveGalleryItem(dormURLS.get(10), rootView, R.id.dussault);
         //retrieveGalleryItem(dormURLS.get(11), rootView, R.id.goller);
-       // retrieveGalleryItem(dormURLS.get(12), rootView, R.id.kennedy);
+        //retrieveGalleryItem(dormURLS.get(12), rootView, R.id.kennedy);
         //retrieveGalleryItem(dormURLS.get(13), rootView, R.id.lincoln);
         retrieveGalleryItem(dormURLS.get(14), rootView, R.id.madonna);
         //retrieveGalleryItem(dormURLS.get(15), rootView, R.id.marian);
         //retrieveGalleryItem(dormURLS.get(16), rootView, R.id.roncalli);
-       // retrieveGalleryItem(dormURLS.get(17), rootView, R.id.sharp);
-       // retrieveGalleryItem(dormURLS.get(18), rootView, R.id.twohy);
+        //retrieveGalleryItem(dormURLS.get(17), rootView, R.id.sharp);
+        //retrieveGalleryItem(dormURLS.get(18), rootView, R.id.twohy);
         retrieveGalleryItem(dormURLS.get(19), rootView, R.id.welch);
+  */
     }
-
+/*
     /**
      * helper function that assigns an image to the resource value given the root view and a url to the image location.
      * this url is meant to be hosted on the WalkGU server
@@ -177,6 +148,7 @@ public class GalleryFragment extends Fragment {
      * @param rootView
      * @param resourceValue
      */
+/*
    private void retrieveGalleryItem(String url, View rootView, int resourceValue){
         Drawable desmet = null;
         try {
@@ -193,6 +165,14 @@ public class GalleryFragment extends Fragment {
         ib.setImageDrawable(drawableResize);
         System.out.println(ib.getWidth());
     }
+*/
+
+    /**
+     * helper function to assign gallery items to imagebuttons
+     * Use the helper function getDormDrawables as the drawables parameter
+     * @param imageButtons
+     * @param drawables
+     */
     private void assignDrawablesToImageButtons(ArrayList<ImageButton> imageButtons, ArrayList<Drawable> drawables){
        ImageButton imageButton;
        Drawable drawable;
@@ -202,6 +182,13 @@ public class GalleryFragment extends Fragment {
            imageButton.setImageDrawable(drawable);
        }
     }
+
+    /**
+     * helper function to retrieve all the drawables for the dorms given an array of urls
+     * returns a list of drawables
+     * @param urls
+     * @return
+     */
     private ArrayList<Drawable> getDormDrawables(String ... urls){
         ArrayList<Drawable> drawables = new ArrayList<>();
         ProgressBar p = (ProgressBar) getActivity().findViewById(R.id.progressBar);
@@ -214,12 +201,25 @@ public class GalleryFragment extends Fragment {
         }
         return resizeDrawables(drawables);
     }
+
+    /**
+     * helper function that resizes all of the drawables.
+     * resizing occures in the for loop.
+     * this is necessary because the image that is retrieved from the server needs to be resized
+     * @param drawables
+     * @return
+     */
     private ArrayList<Drawable> resizeDrawables(ArrayList<Drawable> drawables){
         ArrayList<Drawable> newDrawables = new ArrayList<Drawable>();
         Bitmap bitmap;
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int dimensions = size.x/2 - 65;
         for (int i = 0; i < drawables.size(); i++){
             bitmap = ((BitmapDrawable) drawables.get(i)).getBitmap();
-            newDrawables.add(new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 600, 600, true)));
+            newDrawables.add(new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, dimensions, dimensions, true)));
         }
         return newDrawables;
     }
